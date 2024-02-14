9e1a3cbce689c0444597d311c100ce5f4c5e0b63
miasumon <miasumon.ru@gmail.com>
2024-02-14 16:19:32 +0600

    alpha clash html css done

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,9 @@
+
+
+// function play(){
+//     const homeSection = document.getElementById('home-section');
+//     homeSection.classList.add('hidden');
+
+//     const playgroundSection = document.getElementById('playground-section');
+//     playgroundSection.classList.remove('hidden');
+// }
